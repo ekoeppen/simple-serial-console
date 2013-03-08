c3d9e713edd745cff41af6d64855d65bea636473
Eckhart Kőppen <eck@40hz.org>
2013-03-08 21:58:49 +0200

    Formatting, adding argument for speed, removing soft flow control

--- main.c
+++ main.c
@@ -8,98 +8,112 @@
 #include <sys/types.h>
 #include <sys/time.h>
 
-int transfer_fails( int in, int out ) {
-  unsigned char buffer[512];
+int transfer_fails(int in, int out)
+{
+	unsigned char buffer[512];
 
-  int byte_count = read( in, buffer, sizeof( buffer ) );
-  if ( byte_count == 0 )
-    return 1;
+	int byte_count = read(in, buffer, sizeof(buffer));
+	if (byte_count == 0)
+		return 1;
 
-  write( out, buffer, byte_count );
-  return 0;
+	write(out, buffer, byte_count);
+	return 0;
 }
 
-void configure_input( int in, struct termios *old_options ) {
-  struct termios options;
-  tcgetattr( in, &options );
+void configure_input(int in, struct termios *old_options)
+{
+	struct termios options;
 
-  *old_options = options;
+	tcgetattr(in, &options);
 
-  options.c_cc[VMIN] = 0;
-  options.c_cc[VTIME] = 10;
-  options.c_cflag |= CLOCAL | CREAD | IXON | IXOFF | HUPCL;
-  options.c_lflag = 0;
+	*old_options = options;
 
-  tcflush( in, TCIFLUSH );
-  tcsetattr( in, TCSANOW, &options );
+	options.c_cc[VMIN] = 0;
+	options.c_cc[VTIME] = 10;
+	options.c_cflag |= CLOCAL | CREAD | HUPCL;
+	options.c_lflag = 0;
+
+	tcflush(in, TCIFLUSH);
+	tcsetattr(in, TCSANOW, &options);
 }
 
-void configure_terminal( int terminal ) {
-  struct termios options;
-  tcgetattr( terminal, &options );
+void configure_terminal(int terminal, int baud)
+{
+	struct termios options;
 
-  options.c_oflag &= ~OPOST;
-  options.c_iflag &= ~INLCR & ~ICRNL & ~INPCK;
-  options.c_iflag |= IGNPAR;
-  options.c_cflag &= ~PARENB & ~CSTOPB & ~CSIZE & ~CRTSCTS & ~IXANY;
+	tcgetattr(terminal, &options);
 
-  options.c_cc[VMIN] = 0;
-  options.c_cc[VTIME] = 10;
-  options.c_cflag |= CLOCAL | CREAD | IXON | IXOFF | HUPCL | CS8;
-  options.c_lflag = 0;
+	options.c_oflag &= ~OPOST;
+	options.c_iflag &= ~INLCR & ~ICRNL & ~INPCK;
+	options.c_iflag |= IGNPAR;
+	options.c_cflag &= ~PARENB & ~CSTOPB & ~CSIZE & ~CRTSCTS & ~IXANY;
 
-  tcflush( terminal, TCIFLUSH );
-  tcsetattr( terminal, TCSANOW, &options );
+	options.c_cc[VMIN] = 0;
+	options.c_cc[VTIME] = 10;
+	options.c_cflag |= CLOCAL | CREAD | HUPCL | CS8;
+	options.c_lflag = 0;
+
+	cfsetspeed(&options, baud);
+	tcflush(terminal, TCIFLUSH);
+	tcsetattr(terminal, TCSANOW, &options);
 }
 
-void unconfigure_input( int in, struct termios *old_options ) {
-  tcsetattr( in, TCSANOW, old_options );
+void unconfigure_input(int in, struct termios *old_options)
+{
+	tcsetattr(in, TCSANOW, old_options);
 }
 
-int main( int argc, char **argv ) {
+int main(int argc, char **argv)
+{
+	int in;
+	int out;
+	int terminal;
+	int speed;
+	struct termios old_input_options;
+	struct timeval timeout;
 
-  if ( argc != 2 ) {
-    printf( "Please specify a terminal.\n" );
-    return 1;
-  }
+	if (argc < 2) {
+		printf("Please specify a terminal.\n");
+		return 1;
+	}
+	if (argc == 3)
+		speed = atoi(argv[2]);
+	else
+		speed = 115200;
 
-  int in       = dup( 0 );
-  int out      = dup( 1 );
-  int terminal = open( argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK );
-  struct termios old_input_options;
+	in = dup(0);
+	out = dup(1);
+	terminal = open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
 
-  configure_terminal( terminal );
-  configure_input( in, &old_input_options );
+	configure_terminal(terminal, speed);
+	configure_input(in, &old_input_options);
 
-  for (;;) {
-    fd_set readfds;
-    FD_ZERO( &readfds);
-    FD_SET( terminal, &readfds );
-    FD_SET( in, &readfds );
+	for (;;) {
+		fd_set readfds;
+		FD_ZERO(&readfds);
+		FD_SET(terminal, &readfds);
+		FD_SET(in, &readfds);
 
-    struct timeval timeout;
-    timeout.tv_sec  = 0;
-    timeout.tv_usec = 500000;
+		timeout.tv_sec  = 0;
+		timeout.tv_usec = 500000;
 
-    if ( select( FD_SETSIZE, &readfds, NULL, NULL, &timeout ) == -1 ) {
-      break;
-    } else {
-      if ( FD_ISSET( terminal, &readfds )
-          && transfer_fails( terminal, out ) )
-        break;
+		if (select(FD_SETSIZE, &readfds, NULL, NULL, &timeout) == -1) {
+			break;
+		} else {
+			if (FD_ISSET(terminal, &readfds) && transfer_fails(terminal, out))
+				break;
 
-      if ( FD_ISSET( in, &readfds )
-          && transfer_fails( in, terminal ) )
-        break;
-    }
-  }
+			if (FD_ISSET(in, &readfds) && transfer_fails(in, terminal))
+				break;
+		}
+	}
 
-  unconfigure_input( in, &old_input_options );
+	unconfigure_input(in, &old_input_options);
 
-  close( terminal );
-  close( in );
-  close( out );
+	close(terminal);
+	close(in);
+	close(out);
 
-  return 0;
+	return 0;
 }
 
